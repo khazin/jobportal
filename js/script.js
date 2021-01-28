316bdc0ee828033e16926b93b3051ac99e93ee1e
khazin <khazin316@gmail.com>
2021-01-28 16:52:47 +0800

    create redo register form. not done yet

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-var formApplicant = document.getElementById('formApplicant');
-var formCredential = document.getElementById('formCredential');
-var formBiography = document.getElementById('formBiography');
-var formSkills = document.getElementById('formSkills');
-var formEducation = document.getElementById('formEducation');
-var formExperience = document.getElementById('formExperience');
+const formApplicant = document.getElementById('formApplicant');
+const formEmployer = document.getElementById('formEmployer');
+const formCredential = document.getElementById('formCredential');
+const formBiography = document.getElementById('formBiography');
+const formSkills = document.getElementById('formSkills');
+const formEducation = document.getElementById('formEducation');
+const formExperience = document.getElementById('formExperience');
 
 function nextForm(form1, form2) {
     form1.style.display = 'none';
@@ -17,8 +18,117 @@ function prevForm (form1, form2) {
     
 }
 
-function validateApplicant() {
-    
+function validateEmptyTextField(fieldValue, fieldStr) {
+    if (fieldValue == "") {
+        alert(fieldStr + " must be filled out");
+        return false;
+      }
+    return true;
 }
 
+function validateLengthTextField(fieldValue, fieldName ,min, max) {
+    if ((fieldValue.length < min) || (fieldValue.length > max)) {
+        alert(fieldName + " must be between " + min + " and " + max);
+        return false;
+    }
+    return true;
+}
+
+function validateConfirmPassword(passwordValue, confirmPasswordValue) {
+    if (passwordValue !== confirmPasswordValue) {
+        alert('Password does not match');
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(emailValue) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailValue))
+    {
+      return (true)
+    }
+      alert("You have entered an invalid email address!")
+      return (false)
+}
+
+
+function validateCredentials() {
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    
+    var validateEmptyEmailField = validateEmptyTextField(email, 'Email');
+    var validateEmailFormat = validateEmail(email);
+    var validateEmptyPasswordField = validateEmptyTextField(password, 'Password');
+    var validateEmptyConfirmPasswordField = validateEmptyTextField(confirmPassword, 'Confirm Password');
+    var validatePasswordMatched = validateConfirmPassword(password,confirmPassword);
+
+    let validateResultArr = [
+        validateEmptyEmailField,
+        validateEmailFormat,
+        validateEmptyPasswordField,
+        validateEmptyConfirmPasswordField,
+        validatePasswordMatched
+    ];
+
+    if (validateResultArr.includes(false) != true) {
+            return true;
+    } else {
+            return false;
+    }
+}
+
+
+function validateApplicant() {
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+    // const gender = document.getElementByNames('gender').value;
+    const birthday = document.getElementById('birthday').value;
+    const country = document.getElementById('country').value;
+    const city = document.getElementById('city').value;
+    const job = document.getElementById('job').value;
+    const company = document.getElementById('company').value;
+
+    var validateEmptyFirstnameField = validateEmptyTextField(firstname, 'Firstname');
+    var validateEmptyLastnameField = validateEmptyTextField(lastname, 'Lastname');
+    var validateEmptyBirthdayField = validateEmptyTextField(birthday, 'Birthday');
+    var validateEmptyCountryField = validateEmptyTextField(country, 'Country');
+    var validateEmptyCityField = validateEmptyTextField(city, 'City');
+    var validateEmptyJobField = validateEmptyTextField(job, 'Job');
+    var validateEmptyCompanyField = validateEmptyTextField(company, 'Company');
+
+    let validateResultArr = 
+    [
+        validateEmptyFirstnameField,
+        validateEmptyLastnameField,
+        validateEmptyBirthdayField,
+        validateEmptyCountryField,
+        validateEmptyCityField,
+        validateEmptyJobField,
+        validateEmptyCompanyField
+    ];
+
+    if (validateResultArr.includes(false) != true) {
+        return true;
+} else {
+        return false;
+}
+}
+
+function validateRegisterCredentials(form) {
+    if (validateCredentials() == true) {
+        nextForm(formCredential,formApplicant);
+    } else {
+        return false;
+    }   
+}
+
+function validateRegisterApplicant() {
+    if (validateApplicant() == true) {
+        return true;
+    } else {
+        return false;
+    }   
+}
 var cardContainerApplicant = document.getElementById('cardContainerApplicant');
--- register.php
+++ register.php
@@ -1,23 +1,140 @@
-<?php include './includes/ClassAutoloader.php';?>
+<?php include './includes/ClassAutoloader.php'; ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Job Portal</title>
-      <link rel="stylesheet" href="library/node_modules/bootstrap/dist/css/bootstrap.min.css">
-      <link rel="stylesheet" href="css/style.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   </head>
-   <body>
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Job Portal</title>
+   <link rel="stylesheet" href="library/node_modules/bootstrap/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
    <?php include 'includes/header.php'; ?>
-   <?php include 'includes/register.php'; ?>
-    
-   </body>
-   <script src="js/script.js"></script>
-   <script src="library/node_modules/jquery/dist/jquery.min.js"></script>
+   <div class="container col-12 bg-success d-flex flex-column align-items-center" style="height:100vh">
+      <form action="" method="post" class="form col-6 d-flex flex-column align-items-center">
+
+         <div class="card mt-5" style="width: 38rem;">
+            <nav class="nav nav-pills nav-fill">
+               <a class="nav-link active" href="#" onclick="return nextForm(formEmployer,formApplicant)">For IT Profesionals</a>
+               <a class="nav-link" href="#" onclick="return nextForm(formApplicant,formEmployer)">For employer</a>
+            </nav>
+            <h5 class="mt-3 card-title text-center">Register</h5>
+
+            <!-- form 1 applicant particulars-->
+            <div class="col-12 form__applicant" id="formApplicant">
+               <h6 class="card-subtitle mb-2 text-muted text-center">Fill in your particulars</h6>
+               <div class="d-flex justify-content-between">
+                  <div class="mb-3">
+                     <label for="firstname" class="form-label">Firstname</label>
+                     <input type="text" class="form-control" id="firstname" name="firstname">
+                  </div>
+                  <div class="mb-3">
+                     <label for="lastname" class="form-label">Lastname</label>
+                     <input type="text" class="form-control" id="lastname" name="lastname">
+                  </div>
+               </div>
+
+               <div class="d-flex justify-content-between">
+                  <div class="mt-5">
+                     <input type="radio" id="male" name="gender" value="male">
+                     <label for="male" class="form-label">Male</label>
+                     <input type="radio" id="female" name="gender" value="female">
+                     <label for="female" class="form-label">Female</label>
+                  </div>
+                  <div class="mb-3">
+                     <label for="birthday" class="form-label">Birthday</label>
+                     <input type="date" class="form-control" id="birthday" name="birthday" style="width:205px">
+                  </div>
+               </div>
+
+
+               <div class="d-flex justify-content-between">
+                  <div class="mb-3">
+                     <label for="country" class="form-label">Country</label>
+                     <input type="text" class="form-control" id="country" name="country">
+                  </div>
+                  <div class="mb-3">
+                     <label for="city" class="form-label">City</label>
+                     <input type="text" class="form-control" id="city" name="city">
+                  </div>
+               </div>
+
+               <div class="d-flex justify-content-between">
+                  <div class="mb-3">
+                     <label for="job" class="form-label">Job</label>
+                     <input type="text" class="form-control" id="job" name="job">
+                  </div>
+                  <div class="mb-3">
+                     <label for="company" class="form-label">Company</label>
+                     <input type="text" class="form-control" id="company" name="company">
+                  </div>
+               </div>
+
+               <button type="button" class="btn btn-primary" id="formApplicantNextBtn" name="formApplicantNextBtn"
+               onclick="return nextForm(formApplicant, formCredential)">Next</button>
+
+            </div>
+
+
+
+            <!-- form 2 employer particular-->
+            <div class="card-body col-12 form__employer" id="formEmployer">
+               <h6 class="card-subtitle mb-2 text-muted text-center">Fill in your company details</h6>
+
+               <div class="mb-3">
+                  <label for="companyName" class="form-label">Company Name</label>
+                  <input type="text" class="form-control" id="companyName" name="companyName">
+               </div>
+               <div class="mb-3">
+                  <label for="companyType" class="form-label">Company Type</label>
+                  <input type="text" class="form-control" id="companyType" name="companyType">
+               </div>
+               <div class="mb-3">
+                  <label for="companyAdmin" class="form-label">Company Admin</label>
+                  <input type="text" class="form-control" id="companyAdmin" name="companyAdmin">
+               </div>
+               <div class="mb-3">
+                  <label for="companyContact" class="form-label">Company Contact</label>
+                  <input type="number" class="form-control" id="companyContact" name="companyContact">
+               </div>
+               <button type="button" class="btn btn-primary" id="formEmployerNextBtn" 
+               onclick="return nextForm(formEmployer, formCredential)">Next</button>
+            </div>
+
+            <!-- form 3 credentials-->
+            <div class="card-body col-12 form__credential" id="formCredential">
+               <h6 class="card-subtitle mb-2 text-muted text-center">Fill in your credentials</h6>
+
+               <div class="mb-3">
+                  <label for="email" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="email" name="email">
+               </div>
+               <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="password" name="password">
+               </div>
+               <div class="mb-3">
+                  <label for="confirmPassword" class="form-label">Confirm Password</label>
+                  <input type="password" class="form-control" id="confirmPassword" name="confirmPassword">
+               </div>
+               <button type="button" class="btn btn-primary" id="formApplicantBackBtn" onclick="return prevForm(formApplicant,formCredential)">Back</button>
+
+               <button type="submit" class="btn btn-primary" id="formCredentialSubmitBtn">Confirm</button>
+            </div>
+         </div>
+
+
+      </form>
+   </div>
+</body>
+<script src="js/script.js"></script>
+<script src="library/node_modules/jquery/dist/jquery.min.js"></script>
+
 </html>
 
 <?php
@@ -25,60 +142,60 @@
 ///////////////////////REGISTER APPLICANT//////////////////////////
 
 
-   //  //user model initiated
-   //  $user = new Users();
-    
-   //  //applicant model initiated
-   //  $applicant = new Applicants();
+//  //user model initiated
+//  $user = new Users();
 
-   //  // store user object in user controller
-   //  $userController = new UsersController($user);
+//  //applicant model initiated
+//  $applicant = new Applicants();
 
-   //  // store applicant object in applicant controller
-   //  $applicantController = new ApplicantsController($applicant);
+//  // store user object in user controller
+//  $userController = new UsersController($user);
 
-   //  //set user data
-   //  $userid = 0;
-   //  $userArr = ['appemail','apppasword', 'applicant'];
-   // $userController->setUser($userArr, $userid);
+//  // store applicant object in applicant controller
+//  $applicantController = new ApplicantsController($applicant);
 
-   // $applicantArr = ['firstname','lastname','gender ','dob','country','city','job','compnay'];
-   // //set applicant data
-   // $applicantController->setapplicant($applicantArr, $id = 0);
+//  //set user data
+//  $userid = 0;
+//  $userArr = ['appemail','apppasword', 'applicant'];
+// $userController->setUser($userArr, $userid);
 
-   // $model = new Model();
-   // $modelArr = [$user,$applicant];
+// $applicantArr = ['firstname','lastname','gender ','dob','country','city','job','compnay'];
+// //set applicant data
+// $applicantController->setapplicant($applicantArr, $id = 0);
 
-   //  $controller = new Controller();
-   //  $controller->register($model, $modelArr);
+// $model = new Model();
+// $modelArr = [$user,$applicant];
 
-  
-   ///////////////////////REGISTER EMPLOYER//////////////////////////
+//  $controller = new Controller();
+//  $controller->register($model, $modelArr);
 
 
-    //user model initiated
-    $user = new Users();
-    
-    //employer model initiated
-    $employer = new Employer();
+///////////////////////REGISTER EMPLOYER//////////////////////////
 
-    // store user object in user controller
-    $userController = new UsersController($user);
 
-    // store employer object in employer controller
-    $employerController = new EmployerController($employer);
+// //user model initiated
+// $user = new Users();
 
-    //set user data
-    $userid = 1;
-    $userArr = ['email','pasword','role'];
-   $userController->setUser($userArr, $userid);
+// //employer model initiated
+// $employer = new Employer();
 
-   $employerArr = ['sdgsdg','sdfhe',2352325,'sdfhzdg'];
-   //set employer data
-   $employerController->setEmployer($employerArr, $id = 0);
+// // store user object in user controller
+// $userController = new UsersController($user);
 
-   $model = new Model();
-   $modelArr = [$user,$employer];
+// // store employer object in employer controller
+// $employerController = new EmployerController($employer);
 
-    $controller = new Controller();
-    $controller->register($model, $modelArr);
+// //set user data
+// $userid = 1;
+// $userArr = ['email', 'pasword', 'role'];
+// $userController->setUser($userArr, $userid);
+
+// $employerArr = ['sdgsdg', 'sdfhe', 2352325, 'sdfhzdg'];
+// //set employer data
+// $employerController->setEmployer($employerArr, $id = 0);
+
+// $model = new Model();
+// $modelArr = [$user, $employer];
+
+// $controller = new Controller();
+// $controller->register($model, $modelArr);
